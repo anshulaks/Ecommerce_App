7a672353d50b40bb78e9477092971a94379503f3
AnshulKS <github.com/anshulaks>
2025-01-27 13:46:27 +0000

    Updated connectDB function and added MONGO_URI for production

--- config/db.js
+++ config/db.js
@@ -5,18 +5,40 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URL, {
+        const mongoURI = process.env.MONGO_URI || process.env.MONGO_URL; // Use MONGO_URI for production, fallback to MONGO_URL for local
+        const conn = await mongoose.connect(mongoURI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
-        process.exit(1);
+        process.exit(1); // Exit process with failure
     }
 };
 
 export default connectDB;
+
+
+// import mongoose from 'mongoose';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// const connectDB = async () => {
+//     try {
+//         const conn = await mongoose.connect(process.env.MONGO_URL, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         });
+//         console.log(`MongoDB Connected: ${conn.connection.host}`);
+//     } catch (error) {
+//         console.error(`Error: ${error.message}`);
+//         process.exit(1);
+//     }
+// };
+
+// export default connectDB;
 
 
 
